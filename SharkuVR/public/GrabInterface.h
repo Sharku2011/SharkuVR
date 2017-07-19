d254faa847e9f28e2606d2e75f8451764324ba07
Sharku Jang <Sharku Jang>
2017-07-20 02:30:18 +0900

    그랩 인터페이스 이식
    그랩 인터페이스 이식

--- SharkuVR/public/GrabInterface.h
+++ SharkuVR/public/GrabInterface.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "GrabInterface.generated.h"
 
+class UMaterialInterface;
+
 // This class does not need to be modified.
 UINTERFACE(MinimalAPI)
 class UGrabInterface : public UInterface
@@ -21,6 +23,21 @@ class SHARKUVR_API IGrabInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "GrabInterface")
+	void Grab(class AVRHand* GrabbingHand);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "GrabInterface")
+	void Drop();
+	
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "GrabInterface")
+	void InteractionOn();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "GrabInterface")
+	void InteractionOff();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "GrabInterface")
+	void SetGlow(bool NewBool, UMaterialInterface* GlowMat = nullptr);
 	
 	
 };
