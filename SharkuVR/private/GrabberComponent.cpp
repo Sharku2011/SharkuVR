3e40ce67514fafa98abeb17b660534225f3e7f03
Sharku2011 <pjsw007@naver.com>
2017-07-23 03:48:21 +0900

    grabber component 이식중
    vr hand, destructible morpher 등을 아직 이식하지 않아서 해당 함수 전체 주석처리함

--- SharkuVR/private/GrabberComponent.cpp
+++ SharkuVR/private/GrabberComponent.cpp
@@ -1,18 +1,71 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "GrabberComponent.h"
-
+#include "Kismet/GameplayStatics.h"
+#include "VictoryBPFunctionLibrary.h"
+#include "VRHand.h"
 
 // Sets default values for this component's properties
 UGrabberComponent::UGrabberComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
+
+	GrabType = EGrabType::GT_ATTACH;
+	ObjectName = TEXT("Default");
+
+	DamageMultiplier = 1.0f;
+	DamageBonus = 0.0f;
 }
 
+
+void UGrabberComponent::GrabberSetup()
+{
+	TargetComponent = GetAttachParent();
+
+	bIsTargetPrimitive = IsValid(Cast<UPrimitiveComponent>(TargetComponent));
+
+	if (bIsTargetPrimitive)
+	{
+		GetPrimTargetComponent()->OnComponentHit.AddDynamic(this, &UGrabberComponent::OnPrimTargetCompHit);
+
+		GetPrimTargetComponent()->SetCustomDepthStencilValue(252);
+		if (GrabType == EGrabType::GT_ATTACH)
+		{
+			GetPrimTargetComponent()->OnComponentBeginOverlap.AddDynamic(this, &UGrabberComponent::OnPrimTargetCompBeginOverlap);
+		}
+
+		if (GrabType == EGrabType::GT_HANDLE)
+		{
+			ObjectName = TEXT("Default");
+		}
+
+		if (GrabType == EGrabType::GT_DRAWER)
+		{
+			GetPrimTargetComponent()->SetCollisionResponseToChannel(ECC_GameTraceChannel1, ECR_Ignore);
+
+			SetWorldLocation(GetPrimTargetComponent()->GetCenterOfMass());
+		}
+	}
+}
+
+void UGrabberComponent::Grab_Implementation(AVRHand* NewHand)
+{
+
+}
+
+void UGrabberComponent::Drop_Implementation()
+{
+
+}
+
+void UGrabberComponent::SetGlow_Implementation(bool NewBool, UMaterialInterface* GlowMat)
+{
+
+}
 
 // Called when the game starts
 void UGrabberComponent::BeginPlay()
@@ -20,7 +73,7 @@ void UGrabberComponent::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
+	GrabberSetup();
 }
 
 
@@ -32,3 +85,98 @@ void UGrabberComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 	// ...
 }
 
+void UGrabberComponent::OnPrimTargetCompBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	/*
+	if (OtherActor->IsA(AVRHand::StaticClass()))
+	{
+		return;
+	}
+
+	FVector OverlapedCompHitLocation;
+	FVector OtherCompHitLocation;
+
+	UVictoryBPFunctionLibrary::GetDistanceBetweenComponentSurfaces(OverlappedComp, OtherComp, OverlapedCompHitLocation, OtherCompHitLocation);
+
+	FVector HitDirection = (OtherCompHitLocation - OverlapedCompHitLocation).GetSafeNormal();
+
+	FHitResult TraceHit;
+
+	FCollisionQueryParams CQP;
+
+	OverlappedComp->LineTraceComponent(TraceHit, OverlapedCompHitLocation, OtherCompHitLocation + HitDirection, CQP);
+
+	float Damage = 0;
+
+	if (bIsGrabbed)
+	{
+		Damage = ((GrabbingHand->GetVrVelocity() / 400.0f).SizeSquared() * DamageMultiplier * GrabbingHand->BaseDamage) + DamageBonus;
+
+		GrabbingHand->DrawHitEffect(Damage, TraceHit);
+	}
+
+	UGameplayStatics::ApplyDamage(OtherActor, Damage * 0.9f, nullptr, OverlappedComp->GetOwner(), nullptr);
+	UGameplayStatics::ApplyPointDamage(OtherActor, Damage * 0.1f, HitDirection, TraceHit, nullptr, OverlappedComp->GetOwner(), nullptr);
+
+	if (OtherComp->IsAnySimulatingPhysics())
+	{
+		OtherComp->AddImpulse(HitDirection * Damage, TraceHit.BoneName, true);
+	}
+
+
+	*/
+}
+
+void UGrabberComponent::OnPrimTargetCompEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	//UKismetSystemLibrary::PrintString(TargetComponent, TEXT("EndOverlap"));
+
+
+
+
+}
+
+void UGrabberComponent::OnPrimTargetCompHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	/*
+	ASharkuDestructibleActor* DestActor = Cast<ADestructibleBase>(OtherActor);
+	UDestructibleMorpherComponent* DestMorpherComp = Cast<UDestructibleMorpherComponent>(OtherComp);
+
+	if (IsValid(OtherActor))
+	{
+		float Damage;
+
+		FVector HitVelocity = HitComponent->GetPhysicsLinearVelocityAtPoint(Hit.Location);
+
+		Damage = ((HitVelocity / 400.0f).SizeSquared()  * DamageMultiplier * GetPrimTargetComponent()->GetMass()) + DamageBonus;
+
+		if (FMath::IsNearlyEqual(Damage, 0.0f, 0.001f))
+		{
+			return;
+		}
+
+		if (DestActor && DestActor->GetbIsBroken())
+		{
+			UGameplayStatics::ApplyPointDamage(DestActor, Damage, HitVelocity.GetSafeNormal(), Hit, UGameplayStatics::GetPlayerController(this, 0), TargetComponent->GetOwner(), nullptr);
+
+			return;
+		}
+		else
+		{
+			if (DestMorpherComp)
+			{
+				FDamageEvent DamageEvent;
+				DestMorpherComp->ReceiveComponentDamage(Damage, DamageEvent, UGameplayStatics::GetPlayerController(this, 0), TargetComponent->GetOwner());
+
+				return;
+			}
+			else
+			{
+				UGameplayStatics::ApplyPointDamage(OtherActor, Damage, HitVelocity.GetSafeNormal(), Hit, UGameplayStatics::GetPlayerController(this, 0), TargetComponent->GetOwner(), nullptr);
+				//UKismetSystemLibrary::PrintString(TargetComponent, Hit.BoneName.ToString().Append(FString::Printf(TEXT(" %f Applied"), Damage)));
+				return;
+			}
+		}
+	}
+	*/
+}
--- SharkuVR/public/GrabberComponent.h
+++ SharkuVR/public/GrabberComponent.h
@@ -3,8 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EnumLibrary.h"
 #include "Components/SceneComponent.h"
 #include "GrabberComponent.generated.h"
+
+class AVRHand;
+class AVRPawn;
+class UMeshComponent;
+class UMaterialInterface;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FGrabberBeginInteractionSignature);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FGrabberEndInteractionSignature);
@@ -17,17 +23,124 @@ class SHARKUVR_API UGrabberComponent : public USceneComponent
 	GENERATED_BODY()
 
 public:	
+	// Interaction Signature 시작
+
+	UPROPERTY(BlueprintAssignable, Category = "GrabInterface")
+	FGrabberBeginInteractionSignature OnBeginInteraction;
+
+	UPROPERTY(BlueprintAssignable, Category = "GrabInterface")
+	FGrabberEndInteractionSignature OnEndInteraction;
+
+	UPROPERTY(BlueprintAssignable, Category = "GrabInterface")
+	FGrabberGrabbedSignature OnGrabbed;
+
+	UPROPERTY(BlueprintAssignable, Category = "GrabInterface")
+	FGrabberDroppedSignature OnDropped;
+
+	// Interaction Signature 끝
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	AVRHand* GrabbingHand;
+
+	UPROPERTY(BlueprintReadOnly, VisibleInstanceOnly)
+	UMeshComponent* GlowMesh;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	uint32 bCanGrabbed : 1;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	uint32 bIsGlowing : 1;
+
+protected:
+	// 손에 쥐게 할 타겟 컴포넌트
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "GrabInterface")
+	USceneComponent* TargetComponent;
+
+	// 나중에 Weapon Interface 작업하면서 카테고리 변경할 것"
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GrabInterface")
+	float DamageMultiplier;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GrabInterface")
+	float DamageBonus;
+
+	// 잡는 타입
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "GrabInterface")
+	EGrabType GrabType;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "GrabInterface")
+	FName ObjectName;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "GrabInterface")
+	uint32 bCanTwoHanded : 1;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "GrabInterface")
+	uint32 bCanSmash : 1;
+
+	UPROPERTY(BlueprintReadOnly, Category = "GrabInterface")
+	uint32 bIsTwoHandGrabbed : 1;
+
+	UPROPERTY(BlueprintReadOnly, Category = "GrabInterface")
+	uint32 bIsTargetPrimitive : 1;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = "GrabInterface")
+	uint32 bIsGrabbed : 1;
+
+public:
 	// Sets default values for this component's properties
 	UGrabberComponent();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UFUNCTION(BlueprintCallable, Category = "GrabInterface")
+	void GrabberSetup();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "GrabInterface")
+	void Grab(AVRHand* NewHand);
+	virtual void Grab_Implementation(AVRHand* NewHand);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "GrabInterface")
+	void Drop();
+	virtual void Drop_Implementation();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "GrabInterface")
+	void SetGlow(bool NewBool, UMaterialInterface* GlowMat = nullptr);
+	virtual void SetGlow_Implementation(bool NewBool, UMaterialInterface* GlowMat = nullptr);
+
+	//////////
+
+	UFUNCTION()
+	void OnPrimTargetCompBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnPrimTargetCompEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+	void OnPrimTargetCompHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	//////////
+
+
+	FORCEINLINE EGrabType GetGrabType() const { return GrabType; };
+	FORCEINLINE void SetGrabType(EGrabType NewGrabType) { GrabType = NewGrabType; };
+	FORCEINLINE class USceneComponent* GetTargetComponent() const { return TargetComponent; };
+	FORCEINLINE class UPrimitiveComponent* GetPrimTargetComponent() const { return ((UPrimitiveComponent*)TargetComponent); };
+	FORCEINLINE FName GetObjectName() const { return ObjectName; };
+	FORCEINLINE bool GetCanTwoHanded() const { return bCanTwoHanded; };
+	FORCEINLINE bool GetCanSmash() const { return bCanSmash; };
+	FORCEINLINE bool GetIsTwoHandGrabbed() const { return bIsTwoHandGrabbed; };
+	FORCEINLINE bool GetIsTargetPrimitive() const { return bIsTargetPrimitive; };
+	FORCEINLINE bool GetIsRootComponent(USceneComponent* TargetComp) const { return TargetComp == TargetComp->GetOwner()->GetRootComponent(); };
+	FORCEINLINE bool GetIsGrabbed() const { return bIsGrabbed; };
+	FORCEINLINE bool GetIsGlowing() const { return bIsGlowing; };
+
+protected:	
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+
+
 	
+
+
 };
